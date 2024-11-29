a0a81ca2af11991f47d85c7ca9a33caa9146f165
jake-g <omonoid@gmail.com>
2024-11-29 00:00:44 -0800

    update notebook server scripts

--- start_notebook.sh
+++ start_notebook.sh
@@ -1,6 +1,5 @@
 
 #!/bin/bash
-
 set -e
 
 VENV_DIR=".venv"
@@ -12,10 +11,9 @@ echo "Activating virtual environment..."
 source "$VENV_DIR/bin/activate"
 
 python3 -m pip install --upgrade pip jupyterlab pandas matplotlib scipy numpy
-
 echo https://colab.research.google.com/drive/1mvMnzVS_41HieqnTvfK1vzESBkqOVSvv
 jupyter notebook \
   --NotebookApp.allow_origin='https://colab.research.google.com' \
-  --port=8888 \
+  --port=8887 \
   --NotebookApp.port_retries=0
 

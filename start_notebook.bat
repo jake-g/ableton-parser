df01c5b2fbdcf57b0838313228321ca62aba1eea
jake-g <omonoid@gmail.com>
2024-11-29 00:00:44 -0800

    update notebook server scripts

--- start_notebook.bat
+++
@@ -1,5 +0,0 @@
-pip install jupyter ipykernel jupyter_http_over_ws pandas matplotlib numpy --upgrade
-call jupyter serverextension enable --py jupyter_http_over_ws
-SET mypath=%~dp0
-echo %mypath:~0,-1%
-call jupyter notebook --NotebookApp.allow_origin='https://colab.research.google.com' --port=8887 --NotebookApp.port_retries=0
